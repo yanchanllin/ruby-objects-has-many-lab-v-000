18d25bbd3e60479d314a5e675f38725efa5b7c97
YanC Lin <yonalin1@gmail.com>
2017-11-14 19:33:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,11 @@
 class Artist
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @songs = []
   end
- 
+
   def add_song(song)
     @songs << song
     song.artist = self
@@ -14,7 +14,7 @@ class Artist
     song = Song.new(name, genre)
     @songs << song
     song.artist = self
- 
+   end 
   def songs
     @songs
   end
