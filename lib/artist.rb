3d9a6a709fa6047416dd00e98d62bb05f2747f1a
YanC Lin <yonalin1@gmail.com>
2017-11-14 19:32:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,21 @@
 class Artist
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+ 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    @songs << song
+    song.artist = self
+ 
+  def songs
+    @songs
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor :artist, :name, :genre
- 
+
   def initialize(name, genre)
     @name = name
     @genre = genre
